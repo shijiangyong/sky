133a42d3a0e699e0a0f03c65c61452a80731123e
shijy <1224973308@qq.com>
2018-11-22 10:03:33 +0800

    It's my a easy test

---
+++ mySpringBoot/src/main/java/com/example/demo/service/BatisUserService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+/**
+ * @author shijy
+ * @Date 2018/11/21 15 : 54
+ * @Descriprion
+ */
+public interface BatisUserService {
+    public static void main(String[] args) {
+
+    }
+}
